ea43fdfb76aed59cd715ba22c714d0e6c1450907
= <=>
2018-08-15 20:44:27 +0200

    Add connection handling with scanner

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,18 +29,21 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButtonBinaryMode = new System.Windows.Forms.RadioButton();
+            this.radioButtonASCIIMode = new System.Windows.Forms.RadioButton();
+            this.comboBoxBaudrate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.panelConnectionStatus = new System.Windows.Forms.Panel();
+            this.comboBoxPortCom = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBoxPortCom = new System.Windows.Forms.ComboBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.panelConnectionStatus = new System.Windows.Forms.Panel();
-            this.buttonConnect = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBoxBaudrate = new System.Windows.Forms.ComboBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.label3 = new System.Windows.Forms.Label();
+            this.openFingerprintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.buttonScann = new System.Windows.Forms.Button();
@@ -48,12 +51,9 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.openFingerprintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel1.SuspendLayout();
@@ -78,7 +78,114 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Connection settings";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Connection status";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButtonBinaryMode);
+            this.groupBox2.Controls.Add(this.radioButtonASCIIMode);
+            this.groupBox2.Location = new System.Drawing.Point(148, 16);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(72, 65);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mode";
+            // 
+            // radioButtonBinaryMode
+            // 
+            this.radioButtonBinaryMode.AutoSize = true;
+            this.radioButtonBinaryMode.Checked = true;
+            this.radioButtonBinaryMode.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonBinaryMode.Name = "radioButtonBinaryMode";
+            this.radioButtonBinaryMode.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonBinaryMode.TabIndex = 3;
+            this.radioButtonBinaryMode.TabStop = true;
+            this.radioButtonBinaryMode.Text = "Binary";
+            this.radioButtonBinaryMode.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonASCIIMode
+            // 
+            this.radioButtonASCIIMode.AutoSize = true;
+            this.radioButtonASCIIMode.Location = new System.Drawing.Point(6, 42);
+            this.radioButtonASCIIMode.Name = "radioButtonASCIIMode";
+            this.radioButtonASCIIMode.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonASCIIMode.TabIndex = 2;
+            this.radioButtonASCIIMode.Text = "ASCII";
+            this.radioButtonASCIIMode.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxBaudrate
+            // 
+            this.comboBoxBaudrate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBaudrate.FormattingEnabled = true;
+            this.comboBoxBaudrate.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.comboBoxBaudrate.Location = new System.Drawing.Point(65, 58);
+            this.comboBoxBaudrate.Name = "comboBoxBaudrate";
+            this.comboBoxBaudrate.Size = new System.Drawing.Size(77, 21);
+            this.comboBoxBaudrate.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Baudrate";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(229, 52);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(153, 29);
+            this.buttonConnect.TabIndex = 4;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // panelConnectionStatus
+            // 
+            this.panelConnectionStatus.BackColor = System.Drawing.Color.Red;
+            this.panelConnectionStatus.Location = new System.Drawing.Point(324, 27);
+            this.panelConnectionStatus.Name = "panelConnectionStatus";
+            this.panelConnectionStatus.Size = new System.Drawing.Size(58, 16);
+            this.panelConnectionStatus.TabIndex = 3;
+            // 
+            // comboBoxPortCom
+            // 
+            this.comboBoxPortCom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPortCom.FormattingEnabled = true;
+            this.comboBoxPortCom.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8"});
+            this.comboBoxPortCom.Location = new System.Drawing.Point(65, 27);
+            this.comboBoxPortCom.Name = "comboBoxPortCom";
+            this.comboBoxPortCom.Size = new System.Drawing.Size(77, 21);
+            this.comboBoxPortCom.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port COM";
             // 
             // menuStrip1
             // 
@@ -100,112 +207,22 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // label1
+            // openFingerprintToolStripMenuItem
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Port COM";
+            this.openFingerprintToolStripMenuItem.Name = "openFingerprintToolStripMenuItem";
+            this.openFingerprintToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.openFingerprintToolStripMenuItem.Text = "Open fingerprint";
             // 
-            // comboBoxPortCom
+            // toolStripMenuItem2
             // 
-            this.comboBoxPortCom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxPortCom.FormattingEnabled = true;
-            this.comboBoxPortCom.Items.AddRange(new object[] {
-            "COM1",
-            "COM2",
-            "COM3",
-            "COM4",
-            "COM5",
-            "COM6",
-            "COM7",
-            "COM8"});
-            this.comboBoxPortCom.Location = new System.Drawing.Point(65, 27);
-            this.comboBoxPortCom.Name = "comboBoxPortCom";
-            this.comboBoxPortCom.Size = new System.Drawing.Size(77, 21);
-            this.comboBoxPortCom.TabIndex = 1;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(159, 6);
             // 
-            // radioButton1
+            // exitToolStripMenuItem
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(6, 42);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(52, 17);
-            this.radioButton1.TabIndex = 2;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "ASCII";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // panelConnectionStatus
-            // 
-            this.panelConnectionStatus.BackColor = System.Drawing.Color.Red;
-            this.panelConnectionStatus.Location = new System.Drawing.Point(324, 27);
-            this.panelConnectionStatus.Name = "panelConnectionStatus";
-            this.panelConnectionStatus.Size = new System.Drawing.Size(58, 16);
-            this.panelConnectionStatus.TabIndex = 3;
-            // 
-            // buttonConnect
-            // 
-            this.buttonConnect.Location = new System.Drawing.Point(229, 52);
-            this.buttonConnect.Name = "buttonConnect";
-            this.buttonConnect.Size = new System.Drawing.Size(153, 29);
-            this.buttonConnect.TabIndex = 4;
-            this.buttonConnect.Text = "Connect";
-            this.buttonConnect.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 61);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Baudrate";
-            // 
-            // comboBoxBaudrate
-            // 
-            this.comboBoxBaudrate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxBaudrate.FormattingEnabled = true;
-            this.comboBoxBaudrate.Items.AddRange(new object[] {
-            "9600",
-            "115200"});
-            this.comboBoxBaudrate.Location = new System.Drawing.Point(65, 58);
-            this.comboBoxBaudrate.Name = "comboBoxBaudrate";
-            this.comboBoxBaudrate.Size = new System.Drawing.Size(77, 21);
-            this.comboBoxBaudrate.TabIndex = 6;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.radioButton2);
-            this.groupBox2.Controls.Add(this.radioButton1);
-            this.groupBox2.Location = new System.Drawing.Point(148, 16);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(72, 65);
-            this.groupBox2.TabIndex = 7;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Mode";
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(6, 19);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(54, 17);
-            this.radioButton2.TabIndex = 3;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Binary";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(226, 27);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Connection status";
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
             // 
             // pictureBox1
             // 
@@ -279,24 +296,6 @@
             this.panel2.Size = new System.Drawing.Size(194, 33);
             this.panel2.TabIndex = 11;
             // 
-            // openFingerprintToolStripMenuItem
-            // 
-            this.openFingerprintToolStripMenuItem.Name = "openFingerprintToolStripMenuItem";
-            this.openFingerprintToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
-            this.openFingerprintToolStripMenuItem.Text = "Open fingerprint";
-            // 
-            // toolStripMenuItem2
-            // 
-            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(159, 6);
-            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -316,10 +315,10 @@
             this.Text = "Fingerprint Recognition";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.panel1.ResumeLayout(false);
@@ -340,8 +339,8 @@
         private System.Windows.Forms.Button buttonScann;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButtonBinaryMode;
+        private System.Windows.Forms.RadioButton radioButtonASCIIMode;
         private System.Windows.Forms.ComboBox comboBoxBaudrate;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button buttonConnect;
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using FingerprintRecognition.Utils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,21 +14,225 @@ namespace FingerprintRecognition
 {
     public partial class Form1 : Form
     {
+
+        #region DLL_IMPORTS
+        [DllImport("SFM_SDK.dll",
+            CharSet = CharSet.Auto,
+            EntryPoint = "UF_ReadImage")]
+        static extern int UF_ReadImage(IntPtr image);
+
+        [DllImport("SFM_SDK.dll",
+            CharSet = CharSet.Auto,
+            EntryPoint = "UF_ConvertToBitmap")]
+        static extern IntPtr UF_ConvertToBitmap(IntPtr image);
+
+        [DllImport("SFM_SDK.dll",
+             CharSet = CharSet.Auto,
+             EntryPoint = "UF_SetIdentifyCallback")]
+        static extern void UF_SetIdentifyCallback(IdentifyCallback callback);
+
+        [DllImport("SFM_SDK.dll",
+             CharSet = CharSet.Auto,
+             EntryPoint = "UF_SetEnrollCallback")]
+        static extern void UF_SetEnrollCallback(EnrollCallback callback);
+
+        [DllImport("SFM_SDK.dll",
+             CharSet = CharSet.Auto,
+             EntryPoint = "UF_SetReceiveDataPacketCallback")]
+        static extern void UF_SetReceiveDataPacketCallback(DataCallback callback);
+
+        [DllImport("SFM_SDK.dll",
+             CharSet = CharSet.Auto,
+             EntryPoint = "UF_SetReceiveRawDataCallback")]
+        static extern void UF_SetReceiveRawDataCallback(RawDataCallback callback);
+
+        [DllImport("SFM_SDK.dll",
+             CharSet = CharSet.Auto,
+             EntryPoint = "UF_Enroll")]
+        static extern int UF_Enroll(uint userID, int option, ref uint enrollID, ref byte imageQuality);
+
+        [DllImport("SFM_SDK.dll",
+             CharSet = CharSet.Auto,
+             EntryPoint = "UF_ReadLog")]
+        static extern int UF_ReadLog(int startIndex, int count, IntPtr logRecords, ref int readCount);
+
+        [DllImport("SFM_SDK.dll",
+         CharSet = CharSet.Auto,
+         EntryPoint = "UF_Identify")]
+        static extern int UF_Identify(ref int userID, ref byte subID);
+
+        public delegate void IdentifyCallback(byte errorCode);
+        public delegate void EnrollCallback(byte errorCode, int enrollMode, int numOfSuccess);
+        public delegate void DataCallback(int packetIndex, int numOfPacket);
+        public delegate void RawDataCallback(int transferred, int totalSize);
+
+        private IdentifyCallback m_IdentifyCallback;
+        private EnrollCallback m_EnrollCallback;
+        private IntPtr m_Image;
+
+
+        private void identifyButton_Click(object sender, System.EventArgs e)
+        {
+            labelScannInfo.Text = "Place a finger";
+            labelScannInfo.Refresh();
+
+            int userID = 0;
+            byte subID = 0;
+
+           // toolStripProgressBar1.Value = 0;
+
+            int result = UF_Identify(ref userID, ref subID);
+
+            if (result == 0)
+            {
+                labelScannInfo.Text = "Scann Succeed: " + userID + "(" + subID + ")";
+                labelScannInfo.Refresh();
+            }
+            else
+            {
+                labelScannInfo.Text = "Scann Succes - ";
+                labelScannInfo.Refresh();
+            }
+
+            result = UF_ReadImage(m_Image);
+
+            if (result == 0)
+            {
+                IntPtr hbitmap = UF_ConvertToBitmap(m_Image);
+
+              //  pcbFingerprintScann.SizeMode = PictureBoxSizeMode.Zoom;
+               // pcbFingerprintScann.Image = Image.FromHbitmap(hbitmap);
+            }
+            else if (result == -1)
+            {
+                MessageBox.Show("Error while reading image");
+            }
+        }
+
+        private void enrollButton_Click(object sender, System.EventArgs e)
+        {
+            labelScannInfo.Text = "Place a finger";
+            labelScannInfo.Refresh();
+
+            uint userID = 0;
+            int option = 0x79;
+            uint enrolledID = 0;
+            byte imageQuality = 0;
+
+            //toolStripProgressBar1.Value = 0;
+
+            int result = UF_Enroll(userID, option, ref enrolledID, ref imageQuality);
+
+            if (result == 0)
+            {
+                labelScannInfo.Text = "Enroll Succeed: " + enrolledID + "(" + imageQuality + ")";
+                labelScannInfo.Refresh();
+            }
+            else
+            {
+                labelScannInfo.Text = "Enroll Failed";
+                labelScannInfo.Refresh();
+            }
+
+            result = UF_ReadImage(m_Image);
+
+            if (result == 0)
+            {
+                IntPtr hbitmap = UF_ConvertToBitmap(m_Image);
+
+                //pcbFingerprintScann.Image = Image.FromHbitmap(hbitmap);
+            }
+
+        }
+
+        public void identifyCallback(byte errorCode)
+        {
+            if (errorCode == 0x62)
+            {
+                labelScannInfo.Text = "Scan Success";
+                labelScannInfo.Refresh();
+            }
+        }
+
+        public void enrollCallback(byte errorCode, int enrollMode, int numOfSuccess)
+        {
+            switch (enrollMode)
+            {
+                case 0x30:
+                    break;
+
+                case 0x31:
+                case 0x41:
+                    if (numOfSuccess == 0)
+                    {
+                        label3.Text = "Place the finger again";
+                        label3.Refresh();
+                    }
+
+                    break;
+
+                case 0x32:
+                case 0x42:
+                    break;
+            }
+        }
+
+        public void dataCallback(int packetIndex, int numOfPacket)
+        {
+           // toolStripProgressBar1.Value = (packetIndex + 1) * 100 / numOfPacket;
+        }
+
+        public void rawDataCallback(int transferred, int totalSize)
+        {
+           // toolStripProgressBar1.Value = (transferred + 1) * 100 / totalSize;
+
+        }
+
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
+            
             comboBoxBaudrate.SelectedIndex = 0;
             comboBoxPortCom.SelectedIndex = 0;
+
+            m_Image = Marshal.AllocHGlobal(256 * 1024);
+            m_IdentifyCallback = new IdentifyCallback(identifyCallback);
+            m_EnrollCallback = new EnrollCallback(enrollCallback);
+
+            UF_SetIdentifyCallback(m_IdentifyCallback);
+            UF_SetEnrollCallback(m_EnrollCallback);
+
+            UF_SetReceiveRawDataCallback(new RawDataCallback(rawDataCallback));
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        private void buttonConnect_Click(object sender, EventArgs e)
         {
+            bool isBinaryMode = radioButtonBinaryMode.Checked;
+            string port = comboBoxPortCom.Text;
+            string baudrate = comboBoxBaudrate.Text;
 
+            if(ConnectionUtils.GetInstance().IsConnectedToScanner == false)
+            {
+                if(ConnectionUtils.GetInstance().ConnectToScanner(isBinaryMode, port, baudrate) == -1)
+                {
+                    MessageBox.Show("Can't connect to scanner");
+                }
+            }
+            else
+            {
+                ConnectionUtils.GetInstance().Disconnect();
+            }
+
+            HandleConnectionForms(ConnectionUtils.GetInstance().IsConnectedToScanner);
         }
 
-        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        private void HandleConnectionForms(bool isConnected)
         {
-
+            panelConnectionStatus.BackColor = isConnected ? Color.Lime : Color.Red;
+            buttonConnect.Text = isConnected ? "Disconnect" : "Connect";
+            buttonScann.Enabled = isConnected ? true : false;
         }
     }
 }
